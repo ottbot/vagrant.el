dabf69b7146f8a035bba15285b1fafc1e9ef4b3c
Robert Crim <rob@servermilk.com>
2014-11-25 19:59:12 -0800

    bump patch version

--- vagrant.el
+++ vagrant.el
@@ -1,6 +1,6 @@
 ;;; vagrant.el --- Manage a vagrant box from emacs
 
-;;; Version: 0.5.1
+;;; Version: 0.5.2
 ;;; Author: Robert Crim <rob@servermilk.com>
 ;;; Url: https://github.com/ottbot/vagrant.el
 ;;; Keywords: vagrant chef
