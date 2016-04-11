6d043d8f2c9e0c7039639504a88b2f8cbf519f67
Alex Chesters <alex.chesters@bbc.co.uk>
2016-04-11 08:18:52 +0100

    Update version

--- vagrant.el
+++ vagrant.el
@@ -1,6 +1,6 @@
 ;;; vagrant.el --- Manage a vagrant box from emacs
 
-;;; Version: 0.5.3
+;;; Version: 0.6.0
 ;;; Author: Robert Crim <rob@servermilk.com>
 ;;; Url: https://github.com/ottbot/vagrant.el
 ;;; Keywords: vagrant chef
