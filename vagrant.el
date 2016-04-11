89886eb46739b29a1eb5b8f66dc5096725e8b58b
Alex Chesters <alex.chesters@bbc.co.uk>
2016-04-11 08:17:05 +0100

    Add ability to run vagrant commands from outside sandbox dir

--- vagrant.el
+++ vagrant.el
@@ -30,6 +30,11 @@
   "Options to run vagrant up command"
   :group 'vagrant)
 
+(defcustom vagrant-project-directory "~/vagrant"
+  "The path to a Vagrant sandbox."
+  :group 'vagrant
+  :type 'string)
+
 ;;;###autoload
 (defun vagrant-up ()
   "Bring up the vagrant box."
@@ -101,7 +106,7 @@
 
 (defun vagrant-command (cmd)
   "Run the vagrant command CMD in an async buffer."
-  (let* ((default-directory (file-name-directory (vagrant-locate-vagrantfile)))
+  (let* ((default-directory (file-name-directory (vagrant-locate-vagrantfile vagrant-project-directory)))
          (name (if current-prefix-arg
                    (completing-read "Vagrant box: " (vagrant-box-list)))))
     (async-shell-command (if name (concat cmd " " name) cmd) "*Vagrant*")))
