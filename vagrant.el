11def2d94259cf2287db0bf1ce8f795b8b9e65ea
Giap Tran <txgvnn@gmail.com>
2021-12-06 23:19:23 +0700

    Bump version

--- vagrant.el
+++ vagrant.el
@@ -1,6 +1,6 @@
 ;;; vagrant.el --- Manage a vagrant box from emacs
 
-;;; Version: 0.6.2
+;;; Version: 0.7.0
 ;;; Author: Robert Crim <rob@servermilk.com>
 ;;; Url: https://github.com/ottbot/vagrant.el
 ;;; Keywords: vagrant chef
